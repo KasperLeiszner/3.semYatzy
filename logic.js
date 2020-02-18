1d0cfc778120c242ba0e2b2fc12744e901fa6774
Kasper <leisznern@gmail.com>
2020-02-18 12:02:40 +0100

    Made die and diceCup logic

---
+++ logic.js
@@ -0,0 +1,36 @@
+let amountOfFaces = 6;
+let turn = 1;
+
+let diceCup = {
+    diceArray: [],
+
+    addDie(amount) {
+        for(let i = 0; i < amount; i++) {
+            this.diceArray.push(createDie(amountOfFaces));
+        }
+    },
+
+    rollAll() {
+        for (die of this.diceArray) {
+            die.roll();
+        }
+    }
+} 
+
+function createDie(amountOfFaces) {
+    return {
+        amountOfFaces: amountOfFaces,
+
+        value: 0,
+
+        roll() {
+            this.value = Math.floor(Math.random() * this.amountOfFaces + 1);
+        }
+    }
+}
+
+diceCup.addDie(6);
+diceCup.rollAll();
+console.log(diceCup.diceArray[0].value);
+diceCup.diceArray[0].roll();
+console.log(diceCup.diceArray[0].value);
