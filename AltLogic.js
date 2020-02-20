2acf63b238eb0be10fb9c28cb9aa3c88cc075eef
Thor Henriksen <thor.henriksen.97@gmail.com>
2020-02-20 11:27:36 +0100

    Completed AltLogic.js v1.0

--- AltLogic.js
+++ AltLogic.js
@@ -17,24 +17,146 @@ function throwDice(bools) {
     throwCount++;
 }
 
+function setDice(intArray) {
+    dice = intArray;
+    calcValueFrequency();
+}
+
 function calcValueFrequency() {
-    for(i = 0; i < dice.length; i++) {
+    valueFrequency = [0, 0, 0, 0, 0, 0, 0];
+    for (i = 0; i < dice.length; i++) {
         valueFrequency[dice[i]]++;
     }
 }
 
+function sameValue(value) {
+    return valueFrequency[value] * value;
+}
+
 function onePairPoints() {
-    for(i = valueFrequency.length - 1; i > 0; i--) {
-        if(valueFrequency[i] >= 2) {
-            return i * valueFrequency[i];
+    for (i = valueFrequency.length - 1; i > 0; i--) {
+        if (valueFrequency[i] >= 2) {
+            return i * 2;
         }
     }
     return 0;
 }
 
-throwDice(holds);
+function twoPairPoints() {
+    max = 0;
+    for (i = valueFrequency.length - 1; i > 0; i--) {
+        if (valueFrequency[i] >= 2) {
+            if (max === 0) {
+                max = i * 2;
+            }
+            else {
+                return i * 2 + max;
+            }
+        }
+    }
+    return 0;
+}
+
+function threeOfAKind() {
+    for (i = valueFrequency.length - 1; i > 0; i--) {
+        if (valueFrequency[i] >= 3) {
+            return i * 3;
+        }
+    }
+    return 0;
+}
+
+function fourOfAKind() {
+    for (i = valueFrequency.length - 1; i > 0; i--) {
+        if (valueFrequency[i] >= 4) {
+            return i * 4;
+        }
+    }
+    return 0;
+}
+
+function fullHouse() {
+    threeSame = 0;
+    twoSame = 0;
+    for (i = valueFrequency.length - 1; i > 0; i--) {
+        if (valueFrequency[i] === 3) {
+            threeSame = i * 3
+        }
+        else if (valueFrequency[i] == 2) {
+            twoSame = i * 2;
+        }
+    }
+    if (twoSame !== 0 && threeSame !== 0) {
+        return threeSame + twoSame;
+    }
+    else {
+        return 0;
+    }
+}
+
+function smallStraight() {
+    if (valueFrequency.toString() == [0, 1, 1, 1, 1, 1, 0].toString()) {
+        return 15;
+    }
+    else {
+        return 0;
+    }
+}
+
+function bigStraight() {
+    if (valueFrequency.toString() == [0, 0, 1, 1, 1, 1, 1].toString()) {
+        return 20;
+    }
+    else {
+        return 0;
+    }
+}
+
+function chance() {
+    sum = 0;
+    dice.forEach(value => sum += value)
+    return sum;
+}
+
+function yatzy() {
+    for (i = valueFrequency.length - 1; i > 0; i--) {
+        if (valueFrequency[i] === 5) {
+            return 50;
+        }
+    }
+    return 0;
+}
+
+function possibleScorings() {
+    scorings = [];
+    for (let i = 1; i < 7; i++) {
+        scorings.push(sameValue(i));
+    }
+    pointsFunctions[onePairPoints, twoPairPoints, threeOfAKind, fourOfAKind, fullHouse, smallStraight, 
+        bigStraight, chance, yatzy];
+
+    for(pointsFunction of pointsFunctions) {
+        scorings.push(pointsFunction());
+    }
+
+
+}
+
+// throwDice(holds);
+// console.log(dice);
+// console.log(valueFrequency);
+
+setDice([1, 1, 1, 1, 1]);
 console.log(dice);
 console.log(valueFrequency);
 
-console.log(onePairPoints());
 
+console.log(onePairPoints());
+console.log(twoPairPoints());
+console.log(threeOfAKind());
+console.log(fourOfAKind());
+console.log(fullHouse());
+console.log(smallStraight());
+console.log(bigStraight());
+console.log(chance());
+console.log(yatzy());
