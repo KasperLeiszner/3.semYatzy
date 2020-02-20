a6852fd4e4bbaa52d880bdfccbb42b9406a8e011
Thor Henriksen <thor.henriksen.97@gmail.com>
2020-02-20 09:42:29 +0100

    updated logic.js

--- AltLogic.js
+++ AltLogic.js
@@ -1,4 +1,6 @@
 let dice = [0, 0, 0, 0, 0];
+let valueFrequency = [0, 0, 0, 0, 0, 0, 0];
+let holds = [false, false, false, false, false];
 let throwCount = 0;
 
 function getDice() {
@@ -8,19 +10,31 @@ function getDice() {
 function throwDice(bools) {
     for (let i = 0; i < bools.length; i++) {
         if (!bools[i]) {
-            console.log(i);
             dice[i] = Math.floor(Math.random() * 6) + 1;
         }
     }
+    calcValueFrequency();
     throwCount++;
 }
 
-function onePairPoints() {
-    
+function calcValueFrequency() {
+    for(i = 0; i < dice.length; i++) {
+        valueFrequency[dice[i]]++;
+    }
 }
 
-throwDice([false, false, false, false, false]);
-console.log(values);
+function onePairPoints() {
+    for(i = valueFrequency.length - 1; i > 0; i--) {
+        if(valueFrequency[i] >= 2) {
+            return i * valueFrequency[i];
+        }
+    }
+    return 0;
+}
+
+throwDice(holds);
+console.log(dice);
+console.log(valueFrequency);
 
 console.log(onePairPoints());
 
