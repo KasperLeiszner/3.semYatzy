5befe268ff5b136985b0fa50c9cfb4251606dc7e
Thor Henriksen <thor.henriksen.97@gmail.com>
2020-02-20 09:26:25 +0100

    Updated AltLogic.js

--- AltLogic.js
+++ AltLogic.js
@@ -1,29 +1,22 @@
-let values = [0, 0, 0, 0, 0];
+let dice = [0, 0, 0, 0, 0];
 let throwCount = 0;
 
-function getValues() {
-    return values;
+function getDice() {
+    return dice;
 }
 
 function throwDice(bools) {
-    for (let i = 0; i < values.length; i++) {
+    for (let i = 0; i < bools.length; i++) {
         if (!bools[i]) {
-            values[Math.floor(Math.random() * 6)];
+            console.log(i);
+            dice[i] = Math.floor(Math.random() * 6) + 1;
         }
     }
     throwCount++;
 }
 
 function onePairPoints() {
-    max = 0;
-    faceValue = 0;
-    for(i = 0; i < values.length; i++) {
-        if(values[i] > max) {
-            max = values[i];
-            faceValue = i + 1;
-        }
-    }
-    return faceValue * max;
+    
 }
 
 throwDice([false, false, false, false, false]);
